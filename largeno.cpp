b7be053678a75f2af4af948c03cfe47d806a6207
NawairaSajjad <noorulainsajjad2000@gmail.com>
2023-10-11 20:56:58 +0500

    first commit

---
+++ largeno.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+   int a[] = {4, 9, 1, 3, 8};
+   int largest, i, pos;
+   largest = a[0];
+   for(i=1; i<5; i++) {
+      if(a[i]>largest) {
+         largest = a[i];
+         pos = i;
+      }
+   }
+   cout<<"The largest element in the array is "<<largest<<" and it is at index "<<pos;
+   return 0;
+}
+
